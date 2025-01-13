b33121d5553fa35b40d1a68d8b6b8bc1c1f43122
Hustim <tomasgruber04@gmail.com>
2025-01-13 18:33:31 +0100

    frame rate set

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
-import pygame
+import pygame # type: ignore
 from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
          for event in pygame.event.get():
@@ -13,6 +15,7 @@ def main():
                 return
          pygame.Surface.fill(screen, (0, 0, 0))
          pygame.display.update()
+         dt = clock.tick(60)
 
 
 if __name__ == "__main__":
