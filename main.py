f4fcedae2fa50078cd3c5322c7a748a5ad747a30
Hustim <tomasgruber04@gmail.com>
2025-01-13 19:08:51 +0100

    added player and movement

--- main.py
+++ main.py
@@ -3,19 +3,23 @@
 # throughout this file
 import pygame # type: ignore
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
          for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+         player.update(dt)
          pygame.Surface.fill(screen, (0, 0, 0))
+         player.draw(screen)
          pygame.display.update()
-         dt = clock.tick(60)
+         dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
