f026a7a154b32f6cc7db7c98735aa7af3a473ea9
Hustim <tomasgruber04@gmail.com>
2025-01-13 18:12:43 +0100

    created enviroment to start develop

---
+++ main.py
@@ -0,0 +1,12 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+if __name__ == "__main__":
+    main()
